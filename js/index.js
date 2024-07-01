a81050aca8a8a59bb81c8a2fb1d0226c7b6f22a2
imratatouille <a83284007@gmail.com>
2024-07-01 16:52:44 +0900

    fix

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// index.js
 let currentIndex = 0;
 const slides = document.querySelector(".slides");
 const totalSlides = slides.children.length;
@@ -8,4 +7,4 @@ function showNextSlide() {
     slides.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
-setInterval(showNextSlide, 2000); // 2초마다 다음 슬라이드로 넘어감
+setInterval(showNextSlide, 4000);
